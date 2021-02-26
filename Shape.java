d153cc684117f7a19fb7debc4fe012f1842d58f0
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2021-02-25 17:50:01 -0700

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,14 +1,23 @@
+import javafx.collections.ObservableList;
 import javafx.scene.shape.Polygon;
 
 public class Shape extends Polygon{
 	private double centerX;
 	private double centerY;
-	boolean selected;
+	private boolean selected;
+	private ObservableList<Double> points;
 	Shape() {
 		super();
 	}
 	Shape(double newCenterX, double newCenterY) {
 		super(newCenterX - 50, newCenterY - 50, newCenterX + 50, newCenterY - 50, newCenterX + 50, newCenterY + 50, newCenterX - 50, newCenterY + 50);
+		points = this.getPoints();
+		centerX = newCenterX;
+		centerY = newCenterY;
+	}
+	Shape(double newCenterX, double newCenterY, double[] newPoints) {
+		super(newPoints);
+		points = this.getPoints();
 		centerX = newCenterX;
 		centerY = newCenterY;
 	}
@@ -30,7 +39,16 @@ public class Shape extends Polygon{
 	public void setSelected(boolean select) {
 		selected = select;
 	}
-	public void draw() {
-		
+	public double[] convertPoints(ObservableList<Double> observablePoints) {
+		double[] list = new double[observablePoints.size()];
+		for (int i = 0; i < observablePoints.size(); i++) {
+			list[i] = observablePoints.get(i);
+		}
+		return list;
+	}
+	public Shape draw() {
+		double[] doubleList = convertPoints(getPoints());
+		Shape shape = new Shape(this.getCenterX(), this.getCenterY(), doubleList);
+		return shape;
 	}
 }
