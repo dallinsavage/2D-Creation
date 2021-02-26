1623c608e87c16a5157193fe68810ff98fdd79e4
Dallin Savage <60589051+dallinsavage@users.noreply.github.com>
2021-02-25 17:49:34 -0700

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,7 +10,9 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Display extends Application {
 
@@ -48,14 +51,15 @@ public class Display extends Application {
 			
 			// select shape
 			if (tools.getSelectedToggle() == select) {
-				Shape closest = shapes.get(0);
+				Shape closest = shapes.get(shapes.size() - 1);
 				double clickX = e.getX();
 				double clickY = e.getY();
 				for (int i = 0; i < shapes.size(); i++) {
-					if (Math.abs(shapes.get(i).getCenterX() - clickX) < Math.abs(closest.getCenterX() - clickX) &&
-							Math.abs(shapes.get(i).getCenterY() - clickY) < Math.abs(closest.getCenterY() - clickY)) {
+					if (Math.abs(shapes.get(i).getCenterX() - clickX) > Math.abs(closest.getCenterX() - clickX) &&
+							Math.abs(shapes.get(i).getCenterY() - clickY) > Math.abs(closest.getCenterY() - clickY)) {
 						closest = shapes.get(i);
 					}
+					System.out.println(Math.abs(shapes.get(i).getCenterX() - clickX));
 				}
 				selection[0] = closest;
 				select(selection[0], closest);
@@ -63,25 +67,22 @@ public class Display extends Application {
 			
 			//move selected shape
 			else if (tools.getSelectedToggle() == move) {
-				System.out.println(pane.getChildren().removeAll(shapes));
-				System.out.println(shapes.remove(selection[0]));
-				System.out.println(selection[0].getCenterX());
-				selection[0].setCenterX(e.getX());
-				selection[0].setCenterY(e.getY());
-				System.out.println(selection[0].getCenterX());
-				System.out.println(shapes.add(selection[0]));
-				System.out.println(pane.getChildren().addAll(shapes));
+				Line line = new Line(selection[0].getCenterX(), selection[0].getCenterY(), e.getX(), e.getY());
+				line.setOpacity(0);
+				pane.getChildren().add(line);
+				PathTransition pt = new PathTransition(Duration.millis(1), line, selection[0]);
+				pt.play();
+				pane.getChildren().remove(line);
 			}
 			else if (tools.getSelectedToggle() == resize) {
-				
+				selection[0].setScaleX(Math.abs(selection[0].getCenterX() - e.getX()) / 100);
+				selection[0].setScaleY(Math.abs(selection[0].getCenterX() - e.getX()) / 100);
 			}
 		});
 		newShape.setOnAction(e -> {
 			pane.getChildren().removeAll(shapes);
 			shapes.add((new Shape(375, 375)));
 			pane.getChildren().addAll(shapes);
-		});
-		move.setOnAction(e -> {
 		});
 		color.setOnAction(e -> {
 			vBox.getChildren().addAll(newColor, entry, change);
@@ -93,8 +94,6 @@ public class Display extends Application {
 			vBox.getChildren().removeAll(newColor, entry, change);
 			color.setSelected(false);
 		});
-		resize.setOnAction(e -> {
-		});	
 	}
 	
 	public static void main(String[] args) {
@@ -105,6 +104,5 @@ public class Display extends Application {
 		current.setStroke(current.getFill());
 		newSelection.setSelected(true);
 		newSelection.setStroke(Color.BLACK);
-		newSelection.setStrokeDashOffset(10);
 	}
 }
